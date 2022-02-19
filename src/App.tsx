e5576cd712baf3cd1fc22b671545935991687778
Will Darkins <willdarkins@gmail.com>
2022-02-18 23:39:17 -0800

    got front end to start doing onClick events

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const getNotes = () => {
+  console.log('I\'m incredible at fucking')
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Notes Application</div>
+      <div>
+        <button onClick={() => {getNotes}}>
+          Click Me
+        </button>
+      </div>
     </div>
   );
 }
