5e3320b4701dcd74c81235b338e71f5a44aa35f8
Will Darkins <willdarkins@gmail.com>
2022-02-20 12:46:56 -0800

    created interfaces folder with DUMMY_NOTES to fill out UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-let notesList: any[] = [];
+const [ notesList, setNotesList ] = useState<any []>([])
 const getNotes = async() => {
   try {
     const response = await axios.get(
       `http://localhost:5000/notes`
     )
-    notesList = response.data.notes;
+    //changing state the state variable of notesList to upload data from the API
+    setNotesList(response.data.notes)
   } catch (err) {
     console.error(err)
   }
@@ -25,8 +27,8 @@ const getNotes = async() => {
       </div>
       <div>
         {/* SHOW DETAILS DETAILS OF FIRST ELEMENT */}
-        <h4>{notesList[0].text}</h4>
-        <h5>{notesList[0].link}</h5>
+        <h4>{notesList[0]?.text}</h4>
+        <h5>{notesList[0]?.link}</h5>
       </div>
     </div>
   );
---
+++ src/SEED_NOTES.ts
@@ -0,0 +1,12 @@
+import INote from "./interfaces/note.interface"
+const DUMMY_NOTES: INote[] = [
+        { text: 'Eat breakfast', link: 'https://sharis.com/' },
+        { text: 'Go to work', link: 'https://sharis.com/' },
+        { text: 'Take a break', link: 'https://sharis.com/' },
+        { text: 'Eat lunch', link: 'https://sharis.com/' },
+        { text: 'Drive home', link: 'https://sharis.com/' },
+        { text: 'Eat dinner', link: 'https://sharis.com/' },
+        { text: 'Go to bed', link: 'https://sharis.com/' }
+]
+
+export default DUMMY_NOTES
