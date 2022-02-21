99df74ca94bf631d056cf253c720e7dd04817e3c
Will Darkins <willdarkins@gmail.com>
2022-02-20 19:45:00 -0800

    working out logic for capturing text value from notes to send to database

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, FocusEvent } from 'react'
 import INote from '../../interfaces/note.interface'
 import './Note.css'
 
@@ -6,16 +6,27 @@ import './Note.css'
 type Props = {
     note: INote
 }
+
+//function to grab textcontent from note after user clicks out of div
+const noteTextUpdated = (event: FocusEvent<HTMLDivElement>) => {
+    console.log('Note text has changed')
+    console.log(event.currentTarget.textContent)
+}
+
 //the FC(Functional Component) will take in the Props type and de-construct the note property to pass all the info from INote
-const Note: FC<Props> = ({note}) => {
-  return (
-    <div className='note'>
-    <div className='note__text'>{note.text}</div>
-    <div className='note__link'>
-        <a href={note.link}>{note.link}</a>
-    </div>
-  </div>
-  )
+const Note: FC<Props> = ({ note }) => {
+    return (
+        <div className='note'>
+            <div className='note__text'
+            contentEditable={true}
+            suppressContentEditableWarning={true}
+            onBlur={noteTextUpdated}
+            >{note.text}</div>
+            <div className='note__link'>
+                <a href={note.link}>{note.link}</a>
+            </div>
+        </div>
+    )
 }
 
 export default Note
