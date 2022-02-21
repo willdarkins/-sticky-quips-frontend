215c7a0f08c836b4cf6391ee91e62bc746e66f1e
Will Darkins <willdarkins@gmail.com>
2022-02-20 21:16:02 -0800

    local storage now works with useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,28 @@ import DUMMY_NOTES from './SEED_NOTES';
 import INote from './interfaces/note.interface';
 
 function App() {
-  const [notesList, setNotesList] = useState<any[]>([])
+  const [notesList, setNotesList] = useState<INote[]>([])
 
+  //App component runs first time... BUT if the list from local storage is present we will parse that list...
+  //then use the setNotesList state function to update the state with said list... otherwise it's dummy data
   useEffect(() => {
-    setNotesList(DUMMY_NOTES)
+    const listFromStorage = localStorage.getItem('my-notes')
+    if (listFromStorage) {
+      const listFromStorageArray = JSON.parse(listFromStorage)
+      setNotesList(listFromStorageArray)
+    } else {
+      setNotesList(DUMMY_NOTES)
+    }
   }, [])
+
+  //useEffect hook to save updated notes changes to local storage by creating new variable to stringify notesList array...
+  //then set that variable in the local storage
+  useEffect(() => {
+    console.log('saving to localstorage')
+    const notesListString = JSON.stringify(notesList)
+    localStorage.setItem('my-notes', notesListString)
+  }, [notesList])
+
 
 
   // const getNotes = async() => {
@@ -30,13 +47,14 @@ function App() {
   //the text/links based on their respective ids... if the updatedNote id matches the origin note id, it returns the update...
   //otherwise, the un-edited notes are returned to the array... we then use the setNotesList function from useState to update the array state
   const updateNoteItem = (updatedNote: INote) => {
+    //temporary variable
     const updatedList = notesList.map((noteItem: INote) => {
-      if(noteItem._id === updatedNote._id){
+      if (noteItem._id === updatedNote._id) {
         return updatedNote
       }
       return noteItem
     })
-    setNotesList(updatedList)
+    setNotesList(updatedList) //updating state of notesList
   }
 
   return (
