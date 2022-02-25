d03ba2bb53b281876e9d283931c117e97ce4e5af
Will Darkins <willdarkins@gmail.com>
2022-02-24 21:16:29 -0800

    added modal link and explanation of webpage

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,20 +1,48 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { useState } from 'react'
+import { Button, Container, Modal, Nav, Navbar } from 'react-bootstrap'
 import '../Navbar/NavBar.css'
 
 const NavBar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-<Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Stikcy Quips</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link >About</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Stikcy Quips</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link onClick={handleShow}>About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={show}
+        onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Welcome to Sticky Quips!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>This is an application inspired by an <a href='https://www.youtube.com/watch?v=tPsnzxE-9Gc' target='blank' rel='noopener noreferrer'>episode of The Office</a>
+        where you can keep track of thoughts and ideas, attach links to those thoughts/ideas, and get on with your day! Get started by hitting the
+        black "+" button in the bottom right corner. Enjoy!
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
 
